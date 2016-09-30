d84b05d74b93afc734837dc824135cbc8a383141
antoine <antoine.thach@gmail.com>
2016-09-30 16:29:54 +0200

    Adding a fresh new HTTP server

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const net = require('net');
+// Declare every variable
+var		args;
+const		port;
+var		http;
+var		server;
 
-const server = net.createServer(function(c) {
-    // 'connection' listener
-    console.log('client connected');
+//Getting the first arg and trying to set in a const value - It doesn't work :'(
+args = process.argv.slice(2);
+port = args;
 
-    c.on('data', function (data) {
-	console.log(c.name + "> " + data, c);
-    });
-    c.on('end', function() {
-	console.log('client disconnected');
-    });
-    
-    c.write('hello !');
-    c.pipe(c);
+//Getting the HTTP module
+http = require('http');
+
+//Creating a server with a method "CreateServer", where we give 2 argument : Request & Response
+server = http.createServer(function(req, res) {
+    res.end("Hello my bro");
 });
 
-server.on('error', function(err) {
-    throw err;
-});
+//Listening on the port 1333 - Need to use var in parameters
+server.listen(1333);
 
-server.listen(8125, function() {
-    console.log('server bound');
-});
+//Printing a message on the terminal
+console.log("HTTP Server is running on the port " + 1333);
