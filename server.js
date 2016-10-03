f0eab262559d52ff7687334f5aaa059a092d90e8
antoine <antoine.thach@gmail.com>
2016-10-03 15:04:17 +0200

    Adding a fake download function

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 //Import modules
 
 //Variables
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-`var bodyParser = require('body-parser');
-var fs = require('fs');
-    
+var express	= require('express');        // call express
+var app		= express();                 // define our app using express
+var bodyParser	= require('body-parser');
+var fs		= require('fs');
+var port	= process.env.PORT || 8080;  // set our port
+var route_get;				     // create a route who will use for the get request	
 
 /**
  * Main
  */
 
-console.log("caca");
-
 // configure app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// set our port
-var port = process.env.PORT || 8080;       
-
 // building a route
-var route_get = express.Router();              // get an instance of the express Router
+// get an instance of the express Router
+route_get = express.Router();              
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 route_get.get('/', function(req, res) {
@@ -30,21 +27,29 @@ route_get.get('/', function(req, res) {
 
 //create route plugin
 route_get.get('/plugin', function(req, res) {
-    res.json({ message: 'hooray! welcome to Plugin !' });
 
-    fs.realpath(__dirname, function(err, path) {
+    fs.realpath("./plugin", function(err, path) {
     	if (err) {
 	    console.log(err);
 	    return;
 	}
 	console.log('Path is : ' + path);
+
+	
     });
-    fs.readdir(__dirname, function(err, files) {
+    fs.readdir("./plugin", function(err, files) {
 	if (err) return;
 	files.forEach(function(f) {
 	    console.log('Files: ' + f);
+
+	    test = f.arguments;
+	 
 	});
-    });     
+    });
+//    res.json({ message: 'hooray! welcome to Plugin !'});
+
+    res.sendFile( __dirname + '/server.js'); { root : __dirname }
+    
 });
 
 
