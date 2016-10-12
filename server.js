df9af81d1b91350d5b5208a50c5f10eb25ccbdb2
antoine <antoine.thach@gmail.com>
2016-10-12 16:39:11 +0200

    Adding DB Test

--- server.js
+++ server.js
@@ -194,9 +194,13 @@ var pool = new pg.Pool(config);
 // to run a query we can acquire a client from the pool,
 // run a query on the client, and then return the client to the pool
 pool.connect(function(err, client, done) {
-    /*if(err) {
+    if(err) {
 	return console.error('error fetching client from pool', err);
     }
+
+
+
+
     client.query('SELECT $1::int AS number', ['1'], function(err, result) {
 	//call `done()` to release the client back to the pool
 	done();
@@ -204,18 +208,20 @@ pool.connect(function(err, client, done) {
 	if(err) {
 	    return console.error('error running query', err);
 	}
-	console.log(result.rows[0].number);
+	console.log(result);
+	//	console.log(result.rows[0].number);
 	//output: 1
-    });*/
+    });
+
+    var query = client.query('SELECT login, password, id FROM users');
+    query.on('row', function(row) {
+	console.log('LOGIN : "%s" PASSWORD : "%s" ID : "%s"', row.login, row.password, row.id);
+    });
+
+    
 });
-/*
+
 pool.on('error', function (err, client) {
-    // if an error is encountered by a client while it sits idle in the pool
-    // the pool itself will emit an error event with both the error and
-    // the client which emitted the original error
-    // this is a rare occurrence but can happen if there is a network partition
-    // between your application and the database, the database restarts, etc.
-    // and so you might want to handle it and at least log it out
     console.error('idle client error', err.message, err.stack)
 })
 
@@ -225,5 +231,3 @@ pool.on('error', function (err, client) {
 
 
 
-
-*/
