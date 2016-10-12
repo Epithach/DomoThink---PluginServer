65080e004d18860061d7abe41bd106d803fabdb0
antoine <antoine.thach@gmail.com>
2016-10-12 18:56:26 +0200

    Update : adding route /store/1

--- old/old1/server.js
+++ old/old1/server.js
@@ -1,28 +1,41 @@
 //Import modules
 
 //NODE Variables
-var express	= require('express');		 // call express
-var app		= express();			 // define our app using express
+var express	= require('express');        // call express
+var app		= express();                 // define our app using express
 var bodyParser	= require('body-parser');
 var fs		= require('fs');
-var port	= process.env.PORT || 8080;	// set our port
-var router;					// create a route who will use for the request
-var files	= "";				// Var who will contain the current of the files
+var port	= process.env.PORT || 8080;  // set our port
+var router;				     // create a route who will use for the get request	
+var files	= "";			     // Var who will contain the current of the files	
+var file_list;			     // Var who will contain the list of the files in JSON
+
 
 //PGSQL Variables
-var pg = require('pg');				// call pg	
-var Promise = require('es6-promise').Promise;	// call "Promise", dont know what it is but it work
-var query;					// create a var who will use for the sql query
-var config = {					// CREATE a config to configure the sql connection
-    user: 'root',				//env var: PGUSER
-    database: 'EIP',				//env var: PGDATABASE
-    password: 'root',				//env var: PGPASSWORD
-    host: 'localhost',				// Server hosting the postgres database
-    //port: 5432,				//env var: PGPORT
-    max: 10,					// max number of clients in the pool
+var sql = require('mssql');
+var pg = require('pg');
+
+var Promise = require('es6-promise').Promise;
+
+// create a config to configure both pooling behavior
+// and client options
+// note: all config is optional and the environment variables
+// will be read if the config is not present
+var config = {
+    user: 'root', //env var: PGUSER
+    database: 'EIP', //env var: PGDATABASE
+    password: 'root', //env var: PGPASSWORD
+    host: 'localhost', // Server hosting the postgres database
+    //port: 5432, //env var: PGPORT
+    max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
-var pool = new pg.Pool(config);			//initiate a connection pool who will keep idle connections
+
+//this initializes a connection pool
+//it will keep idle connections open for a 30 seconds
+//and set a limit of maximum 10 idle clients
+var pool = new pg.Pool(config);
+
 
 
 /**
@@ -50,6 +63,17 @@ router.get('/', function(req, res) {
 //create route plugin
 router.get('/plugin', function(req, res) {
 
+
+    /*fs.realpath("./plugin", function(err, path) {
+    	if (err) {
+	    console.log(err);
+	    res.statusCode = 500;
+	    return;
+	}
+	console.log('Path is : ' + path);
+    });*/
+
+    
     fs.readdir("./plugin", function(err, files) {
 	if (err) {
 	    res.statusCode = 500;
@@ -62,10 +86,7 @@ router.get('/plugin', function(req, res) {
 
 });
 
-/***
-* PLUGIN ROUTE (Need to find how to create dynamic route)
-*/
-
+// PLUGIN
 router.get('/plugin/plugin_1', function(req, res) {
     res.statusCode = 200;
     console.log("Show information from plugin_1 from /plugin");
@@ -81,12 +102,24 @@ router.get('/plugin/plugin_3', function(req, res) {
     console.log("Show information from plugin_3 from /plugin");
 });
 
-/***
-* STORE ROUTE
-*/
 
-// A route who will list evey plugin from the store
+
+
+// ROUTE STORE
 router.get('/store', function(req, res) {
+
+    
+/*    fs.readdir("./store", function(err, files) {
+	if (err) {
+	    res.statusCode = 500;
+	    return;
+	}
+	res.json(files);
+	res.statusCode = 200;
+	console.log(files);
+    });
+*/    
+    
 
     res.writeHead(200, { 'Content-Type': 'application/json'});
 
@@ -97,54 +130,60 @@ router.get('/store', function(req, res) {
 	    return console.error('error fetching client from pool', err);
 	}
 
-	query = client.query('select id, name, info, creator from store', function(err, result) {
+//	var query = client.query('select id, name, info, creator from store')
+
+	var query = client.query('select id, name, info, creator from store', function(err, result) {
 	    if (err) {
 		result.writeHead(200, { 'Content-Type': 'application/json'});
 		return console.error('SQL Query failed');
 	    }
+
 	    console.log(result.rows);
 	    res.end(JSON.stringify(result.rows));
 	});
-    });
-    pool.on('error', function (err, client) {
-	console.error('idle client error', err.message, err.stack)
-    })
-    
-    
-});
 
-router.get('/store/1', function(req, res) {
+/*
+	query.on('row', function(row) {
+	    console.log('ID : "%s" NAME : "%s" INFO : "%s" CREATOR : "%s"',
+			row.id,
+			row.name,
+			row.info,
+			row.creator);
 
-    pool.connect(function(err, client, done) {
-	if(err) {
-	    return console.error('error fetching client from pool', err);
-	}
+//	    res.end(JSON.stringify(row));
+	});*/
+
+
+
 	
-	query = client.query('select id, name, info, creator from store where id=1', function(err, result) {
-	    if (err) {
-		result.writeHead(200, { 'Content-Type': 'application/json'});
-		return console.error('SQL Query failed');
-	    }
-	    console.log(result.rows);
-	    res.json(result.rows);
-	});
     });
+    
     pool.on('error', function (err, client) {
 	console.error('idle client error', err.message, err.stack)
     })
+    
+    
 });
 
-//Download plugin_1
+router.get('/store/plugin_1', function(req, res) {
+    res.statusCode = 200;
+    res.sendFile( __dirname + '/store/plugin_1'); { root : __dirname }
+});
+
+router.get('/store/plugin_2', function(req, res) {
+    res.statusCode = 200;
+    res.sendFile( __dirname + '/store/plugin_2'); { root : __dirname }
+});
+
+router.get('/store/plugin_3', function(req, res) {
+    res.statusCode = 200;
+    res.sendFile( __dirname + '/store/plugin_3'); { root : __dirname }
+});
+
 router.post('/store/install/plugin_1', function(req, res) {
     res.statusCode = 200;
     console.log("Install plugin_1 from /store");
     res.sendFile( __dirname + '/store/install/plugin_1'); { root : __dirname }
-});
-
-//Download plugin_2
-router.post('/store/plugin_2', function(req, res) {
-    res.statusCode = 200;
-    res.sendFile( __dirname + '/store/install/plugin_2'); { root : __dirname }
 });
 
 router.post('/store/uninstall/plugin_1', function(req, res) {
